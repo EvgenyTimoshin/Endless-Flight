15ecabfc6d5380ee8ca2de8477d26cab25553e02
EvgenyTimoshin <evgenytimoshin@gmail.com>
2017-11-30 22:04:56 +0100

    Testing Unity

---
+++ Endless-Flight/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    
+	// Use this for initialization
+	void Start ()
+	{
+
+	    GetComponent<Animation>().Play("TakeOff");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

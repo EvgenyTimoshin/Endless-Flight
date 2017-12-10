e82a26d3f9aa53d5b9aabaec637858ee361aae51
EvgenyTimoshin <evgenytimoshin@gmail.com>
2017-12-10 18:33:43 +0100

    Plane Movement Implemented
    Implemented the planes movement on the X axis. Including the planes rotation.

--- Endless-Flight/Assets/Scripts/Player.cs
+++ Endless-Flight/Assets/Scripts/Player.cs
@@ -8,58 +8,96 @@ public class Player : MonoBehaviour
 {
     Rigidbody rb;
     const float minimumSpeed = -1500;
-    const float maxHorizontalSpeed = 5000;
+    const float maxHorizontalSpeed = 250;
     float currentHorizontalSpeed = 0;
+    private float currentRotation = 0;
+    private Vector3 currentAngle;
+
     //float maxHorizontalSpeed = 5000;
     float bonusHorizontalSpeed = 0;
     float boostHorizontalSpeed = 0;
+
+    private bool controlZone = true;
  
 
     // Use this for initialization
     void Start ()
 	{
 	    rb = GetComponent<Rigidbody>();
-	    //Animator anim = GetComponent<Animator>();
-	    //anim.Play("TakeOff");
+	    currentAngle = rb.transform.eulerAngles;
+        //Animator anim = GetComponent<Animator>();
+        //anim.Play("TakeOff");
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-	    rb.AddRelativeForce(Vector3.forward * 25 * Time.deltaTime * 50);
+	    rb.AddRelativeForce(Vector3.forward  * Time.deltaTime * 50);
 
 	    if (Input.GetKey(KeyCode.A))
 	    {
-	        transform.Rotate(0, 0, Time.deltaTime * 10);
-	        currentHorizontalSpeed = Mathf.Lerp(currentHorizontalSpeed, maxHorizontalSpeed + bonusHorizontalSpeed + boostHorizontalSpeed, Time.deltaTime / 0.2f);
+            //transform.Rotate(0, 0, Time.deltaTime * 50);
+            //currentRotation = Mathf.Lerp(45,currentRotation, Time.deltaTime * 50 );
+
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, 45, Time.deltaTime));
+
+            currentHorizontalSpeed = Mathf.Lerp(currentHorizontalSpeed, -maxHorizontalSpeed + -bonusHorizontalSpeed + -boostHorizontalSpeed, Time.deltaTime );
+            
 	    }
 
         if (Input.GetKey(KeyCode.D))
 	    {
-            transform.Rotate(0,0,-Time.deltaTime * 10);
-	        //currentHorizontalSpeed = Mathf.Lerp(currentHorizontalSpeed, -maxHorizontalSpeed + -bonusHorizontalSpeed + -boostHorizontalSpeed, Time.deltaTime / 0.2f);
-	    }
+            // transform.Rotate(0,0,-Time.deltaTime * 50);
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, -45, Time.deltaTime));
+
+            currentHorizontalSpeed = Mathf.Lerp(currentHorizontalSpeed, maxHorizontalSpeed + bonusHorizontalSpeed + boostHorizontalSpeed, Time.deltaTime );
+        }
 
 	    if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
 	    {
 	        currentHorizontalSpeed = Mathf.Lerp(currentHorizontalSpeed, 0, Time.deltaTime / 0.1f);
-	    }
-        /*
-	    if (transform.position.x < -3800)
+            // currentRotation = Mathf.Lerp(0, rb.rotation.z, Time.deltaTime * 50);
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, 0, Time.deltaTime * 2));
+        }
+        
+	    if (transform.position.x < 150)
 	    {
-	        transform.position = new Vector3(-3800, transform.position.y, transform.position.z);
+	        transform.position = new Vector3(150, transform.position.y, transform.position.z);
 	        currentHorizontalSpeed = 0;
-	    }
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, 0, Time.deltaTime * 2));
+            Input.ResetInputAxes();
+             
+        }
 
-	    if (transform.position.x > -630)
+	    if (transform.position.x > 350)
 	    {
-	        transform.position = new Vector3(-630, transform.position.y, transform.position.z);
+	        transform.position = new Vector3(350, transform.position.y, transform.position.z);
 	        currentHorizontalSpeed = 0;
-	    }
-        */
+	        currentAngle = new Vector3(
+	            Mathf.LerpAngle(currentAngle.x, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.y, 0, Time.deltaTime),
+	            Mathf.LerpAngle(currentAngle.z, 0, Time.deltaTime * 2));
+            Input.ResetInputAxes();
+        }
+        
 
 	    rb.velocity = new Vector3(currentHorizontalSpeed, rb.velocity.y, rb.velocity.z);
+        //rb.rotation = new Quaternion(rb.rotation.x, rb.rotation.y, currentRotation, rb.rotation.w);
+        //rb.rotation.Set(rb.rotation.x, rb.rotation.y, 45, rb.rotation.w);
+	    rb.transform.eulerAngles = currentAngle;
 
 
     }

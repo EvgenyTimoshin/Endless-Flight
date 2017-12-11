02b2eaf9b0be7e79d060f55f97066d39c1e96d3c
EvgenyTimoshin <evgenytimoshin@gmail.com>
2017-12-11 20:40:48 +0100

    Configured new camera + skybox/water
    Configured the new camera to follow player but not to rotate with player  for clearer gameplay nad UI score (up for change), Also added and configured new skybox and water (Configured both for Performance)

---
+++ Endless-Flight/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 20, player.transform.position.z - 50);
+	}
+}

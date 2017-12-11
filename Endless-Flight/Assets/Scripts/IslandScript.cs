a939b8a4e93a2bdb1fb5ceb4022452a37d60f0c6
EvgenyTimoshin <evgenytimoshin@gmail.com>
2017-12-11 15:56:00 +0100

    Working Alpha Prototpye
    A working version with random islands spawning in as the plane progresses through the game world. Also added advanced water (Needs to be tested for performance on mobile devices)

--- Endless-Flight/Assets/Scripts/IslandScript.cs
+++ Endless-Flight/Assets/Scripts/IslandScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework.Constraints;
 using UnityEngine;
+using Random = System.Random;
 
 public class IslandScript : MonoBehaviour {
 
@@ -34,6 +35,26 @@ public class IslandScript : MonoBehaviour {
 
     void Spawn_Next_Island()
     {
-        Instantiate((Resources.Load("Islands/Island2")), new Vector3(0, 0, transform.position.z + 1600), new Quaternion(0, 0, 0, 0));
+        Random rnd = new Random();
+        int choice = rnd.Next(1, 4);
+        String islandChoice = "Island/Island2";
+
+        if (choice == 1)
+        {
+            islandChoice = "Islands/Island2";
+        }
+        if (choice == 2)
+        {
+            islandChoice = "Islands/Island3";
+        }
+        if (choice == 3)
+        {
+            islandChoice = "Islands/Island4";
+        }
+        if (choice == 4)
+        {
+            islandChoice = "Islands/Island5";
+        }
+        Instantiate((Resources.Load(islandChoice)), new Vector3(0, 0, transform.position.z + 1600), new Quaternion(0, 0, 0, 0));
     }
 }

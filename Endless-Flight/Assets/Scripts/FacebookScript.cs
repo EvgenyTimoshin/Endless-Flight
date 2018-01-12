9768142aff55ec384d54e1dd4a32b77c5963bb88
AmyKelly56 <amyannakelly@outlook.com>
2018-01-13 00:00:28 +0100

    Facebook Script
    Creation of Facebook Script for Login and Logout Methods.

---
+++ Endless-Flight/Assets/Scripts/FacebookScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FacebookScript : MonoBehaviour {
+
+	private void Awake()
+	{
+		if (!FB.IsInitialized) {
+			FB.Init (() => {
+				if (FB.IsInitialized)
+					FB.ActivateApp ();
+				else
+					Debug.LogError ("Couldn't initialise");
+			},
+
+				isGameShown => {
+					if (!isGameShown)
+						Time.timeScale = 0;
+					else
+						Time.timeScale = 1;
+				});
+					
+		} else
+			FB.ActivateApp (); 
+	}
+
+	//Login Method
+	#region Login / Logout
+	public void FacebookLogin() 
+	{
+		var permissions = new List<string> () { "public_profile", "email", "user_friends" };
+		FB.LogInWithReadPermissions (permissions);
+
+	}
+
+	public void FacebookLogout()
+	{
+		FB.LogOut ();
+	}
+}

5bb7b9184ed43319d17b9626dd466fb129c6bb35
AmyKelly56 <amyannakelly@outlook.com>
2018-02-02 22:17:46 +0100

    Facebook Intergration
    Addition of Android SDK and NDK required for later use of scores API

---
+++ Endless-Flight/Assets/Scripts/TestLogin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity; 
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TestLogin : MonoBehaviour {
+
+	void Awake() {
+		if (!FB.IsInitialized) {
+			FB.Init (InitCallBack);
+		}
+	}
+
+	void InitCallBack() {
+		Debug.Log ("FB has been initialised");
+	}
+
+	public void Login() {
+		if (FB.IsLoggedIn) {
+			FB.LoginWithReadPermissions (new List<string> { "user_friends" }, LoginCallBack);
+
+		}
+	}
+
+	void LoginCallBack(ILoginResult result) {
+		if(result.Error == null) {
+			Debug.Log ("FB logged in");
+		}
+		else {
+			Debug.Log ("Error during login: " + result.Error);
+		}
+	}
+
+}
